a71af2e5a70115390e0523322b23b691600f6bdd
Dan Du <dudan@Dans-MacBook-Pro.local>
2018-08-16 13:12:23 +0700

    Fix namespace bug

--- config/app.php
+++ config/app.php
@@ -154,11 +154,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        DatLichVietAPI\Providers\AppServiceProvider::class,
+        DatLichVietAPI\Providers\AuthServiceProvider::class,
+        // DatLichVietAPI\Providers\BroadcastServiceProvider::class,
+        DatLichVietAPI\Providers\EventServiceProvider::class,
+        DatLichVietAPI\Providers\RouteServiceProvider::class,
 
     ],
 
